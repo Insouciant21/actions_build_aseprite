4abb3fe22e19ba8d3774864ce0d399b7b8cb2e3b
Insouciant21 <48847364+Insouciant21@users.noreply.github.com>
2020-06-07 10:04:28 +0800

    Update linux.toolchain.cmake

--- linux.toolchain.cmake
+++ linux.toolchain.cmake
@@ -1,5 +1,5 @@
 cmake_minimum_required( VERSION 2.6.3 )
 set(CMAKE_SYSTEM_NAME Linux )
  
-SET (CMAKE_C_COMPILER             "/usr/bin/clang-9")
-SET (CMAKE_CXX_COMPILER             "/usr/bin/clang++-9")
+SET (CMAKE_C_COMPILER             "/usr/bin/gcc-9")
+SET (CMAKE_CXX_COMPILER             "/usr/bin/g++-9")
