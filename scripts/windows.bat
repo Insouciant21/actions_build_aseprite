af1a0e4d2fba274ade6c7425285278f1d06842f1
Insouciant21 <48847364+Insouciant21@users.noreply.github.com>
2020-06-07 13:00:24 +0800

    Update windows.bat

--- scripts/windows.bat
+++ scripts/windows.bat
@@ -1,5 +1,5 @@
 powershell.exe ./install.ps1
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat" -arch=x64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat" -arch=x64
 cd Windows
 set workdir=chdir
 cd aseprite
