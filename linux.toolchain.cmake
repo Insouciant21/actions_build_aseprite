6a931a8d383662cacfe43f6ebb1f8f45582a15ec
Insouciant21 <48847364+Insouciant21@users.noreply.github.com>
2020-06-07 08:02:29 +0800

    Update linux.toolchain.cmake

--- linux.toolchain.cmake
+++ linux.toolchain.cmake
@@ -1,14 +1,14 @@
 cmake_minimum_required( VERSION 2.6.3 )
 set(CMAKE_SYSTEM_NAME Linux )
  
-SET (CMAKE_C_COMPILER             "/usr/bin/clang")
+SET (CMAKE_C_COMPILER             "/usr/bin/clang-9")
 SET (CMAKE_C_FLAGS                "-Wall ")
 SET (CMAKE_C_FLAGS_DEBUG          "-g")
 SET (CMAKE_C_FLAGS_MINSIZEREL     "-Os -DNDEBUG")
 SET (CMAKE_C_FLAGS_RELEASE        "-O4 -DNDEBUG")
 SET (CMAKE_C_FLAGS_RELWITHDEBINFO "-O2 -g")
  
-SET (CMAKE_CXX_COMPILER             "/usr/bin/clang++")
+SET (CMAKE_CXX_COMPILER             "/usr/bin/clang++-9")
 SET (CMAKE_CXX_FLAGS                "-Wall")
 SET (CMAKE_CXX_FLAGS_DEBUG          "-g")
 SET (CMAKE_CXX_FLAGS_MINSIZEREL     "-Os -DNDEBUG")
