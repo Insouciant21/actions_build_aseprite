c91483980b320f97d2b5638cfcc0ba476f160446
Insouciant21 <2964318290@qq.com>
2020-06-07 17:14:29 +0800

    styles: add some notes

--- scripts/win/dependencies.ps1
+++ scripts/win/dependencies.ps1
@@ -2,14 +2,17 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 # Install dependencies
 choco install cmake visualstudio2019community ninja windows-sdk-10.1 git jq grep curl wget 7zip -y
-# Get source code
+# Environment
 mkdir tmp
 cd tmp
+# Get source code
 $source=$(curl https://api.github.com/repos/aseprite/aseprite/releases/latest | Select -ExpandProperty Content | jq -r '.assets[].browser_download_url')
 $skia=$(curl https://api.github.com/repos/aseprite/skia/releases/latest | Select -ExpandProperty Content| jq -r '.assets[].browser_download_url' | findstr Windows | findstr 64)
 iwr -outf source.zip $source
 iwr -outf skia.zip $skia
+# Unzip the source code
 7z x source.zip -oaseprite | findstr ing 
 7z x skia.zip -oskia | findstr ing 
+# Directory for build
 cd aseprite
 mkdir build
--- scripts/win/windows.bat
+++ scripts/win/windows.bat
@@ -1,11 +1,15 @@
+:: Using powershell to prepare dependencies
 powershell.exe ./dependencies.ps1
+:: Call Developer Command Prompt
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat" -arch=x64
+:: Build
 cd tmp
 for /F %%i in ('chdir') do ( set workdir=%%i)
 cd aseprite
 cd build
 cmake -DCMAKE_C_COMPILER=cl -DCMAKE_CXX_COMPILER=cl -DCMAKE_BUILD_TYPE=Release -DLAF_BACKEND=skia -DSKIA_DIR=%workdir%\skia -DSKIA_LIBRARY_DIR=%workdir%\skia\out\Release-x64 -G Ninja ..
 ninja aseprite
+:: 7z
 cd bin
 7z a aseprite-win64.zip * -mx0
 mv aseprite-win64.zip %workdir%\..
